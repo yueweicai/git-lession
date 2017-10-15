f4a86e957c1bbf1eeddf2b879edee1efe1d45e58
yueweicai <weicai.yue@orilore.com>
2017-10-15 16:09:35 +0800

    这是一个关于Git的学习案例

---
+++ src/main/java/com/orilore/yue/App.java
@@ -0,0 +1,13 @@
+package com.orilore.yue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/orilore/yue/Test.java
@@ -0,0 +1,9 @@
+package com.orilore.yue;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Github!");
+	}
+
+}
